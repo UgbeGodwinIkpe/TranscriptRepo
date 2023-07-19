53357456aae7d97a7cf273d478d9b897a08e9ebf
manoahLinks <manoahluka@gmail.com>
2023-07-19 01:14:55 +0100

    changing header fontsize

--- client/src/containers/header/Header.jsx
+++ client/src/containers/header/Header.jsx
@@ -14,7 +14,7 @@ function Header() {
   return (
     <div className='flex-1 grid md:grid-cols-2 grid-cols-1 gap-y-[25px] md:gap-y-0'>
         <div className='flex flex-col bg-white justify-center gap-y-4'>
-            <h1 className='md:text-[60px] text-[25px] font-bold leading-none'>Your Transcripts, Simplified Access, Request, Delivered</h1>
+            <h1 className='md:text-[60px] text-[30px] font-bold leading-none'>Your Transcripts, Simplified Access, Request, Delivered</h1>
             <p className='md:text-[16px] text-[10px]  font-light'>Accelerate rour educational journey with our simplified transcript retrieval platform. Gain easy access to your transcripts, effortlessly request official records, and pave the way to your success.</p>
             <Link to={'/selectlogin'}>
                 <Button
