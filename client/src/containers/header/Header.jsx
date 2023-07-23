5a079107f3dd5bbdfc0d0280dd7a7204e0d84330
manoahLinks <manoahluka@gmail.com>
2023-07-23 01:10:41 +0100

    additional change to header container

--- client/src/containers/header/Header.jsx
+++ client/src/containers/header/Header.jsx
@@ -10,14 +10,12 @@ import { Link } from 'react-router-dom'
 // assets imports
 import headerImg from '../../assets/headerimg.png'
 
-import './header.css';
-
 function Header() {
   return (
-    <div className='flex-1 grid md:grid-cols-2 grid-cols-1 gap-y-[25px] md:gap-y-0'>
+    <div className='flex-1 grid md:grid-cols-2 grid-cols-1'>
         <div className='flex flex-col bg-white justify-center gap-y-4'>
             <h1 className='md:text-[60px] text-[30px] font-bold leading-none'>Your Transcripts, Simplified Access, Request, Delivered</h1>
-            <p className='md:text-[16px] text-[10px]  font-light'>Accelerate rour educational journey with our simplified transcript retrieval platform. Gain easy access to your transcripts, effortlessly request official records, and pave the way to your success.</p>
+            <p className='md:text-[16px] text-[8px]  font-light'>Accelerate rour educational journey with our simplified transcript retrieval platform. Gain easy access to your transcripts, effortlessly request official records, and pave the way to your success.</p>
             <Link to={'/selectlogin'}>
                 <Button
                   variant='contained'
@@ -28,7 +26,7 @@ function Header() {
             </Link>
         </div>
 
-        <div className='flex slide-fwd-center'>
+        <div className='flex'>
             <img src={headerImg} alt="headerimg" />
         </div>
     </div>
