2e1f7d42f67d7544eecac8e0ea2becf8f5a0df3b
manoahLinks <manoahluka@gmail.com>
2023-08-02 21:35:58 +0100

    working on selectlogin component responsiveness

--- client/src/components/selectLogin/SelectLogin.jsx
+++ client/src/components/selectLogin/SelectLogin.jsx
@@ -13,32 +13,32 @@ import { Link } from 'react-router-dom'
 
 function SelectLogin() {
   return (
-    <div className='flex flex-1 justify-center items-center justify-items-center'>
+    <div className='md:overflow-y-hidden overflow-y-scroll flex flex-1 justify-center justify-items-center'>
         <div className="p-5 rounded-lg md:w-8/12 w-full gap-y-8 flex flex-col">
 
-            <h4 className='text-center'>Continue as</h4>
+            <h4 className='text-center font-bold'>Continue as</h4>
 
             <div className='grid md:grid-cols-2 grid-cols-1 justify-between gap-y-4 md:gap-y-0 md:gap-x-16'>
-                <div className="flex flex-col items-center">
+                <div className="border p-2 gap-y-2 rounded-md flex flex-col items-center">
                     <img src={university} alt="unipic" />
                     <h4>university</h4>
                     <Link to={`/institution/login`}>
                         <Button
                             variant='contained'
-                            className='bg-[#6B3FA0] mt-4 md:mt-8'
+                            className='bg-[#6B3FA0] hover:bg-white hover:text-[#6B3FA0]'
                         >
                             continue
                         </Button>
                     </Link>
                 </div>
 
-                <div className="flex flex-col items-center">
+                <div className="border p-2 gap-y-2 rounded-md flex flex-col items-center">
                     <img src={alumni} alt="alumnipic" />
                     <h4>Alumni</h4>
                     <Link to={`/alumni/login`}> 
                         <Button
                             variant='contained'
-                            className='border border-[#6B3FA0] bg-white text-[#6B3FA0] mt-4 md:mt-8'
+                            className='hover:text-white hover:bg-[#6B3FA0] bg-white text-[#6B3FA0]'
                         >
                             continue
                         </Button>
