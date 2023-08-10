630a5a0f97aa8cb7f5f00958d2f64f312cf4eb24
manoahLinks <manoahluka@gmail.com>
2023-08-10 19:06:52 +0100

    setting bussiness logic for institution signup

--- client/src/pages/Institution/Auth/signup/Signup.jsx
+++ client/src/pages/Institution/Auth/signup/Signup.jsx
@@ -1,34 +1,60 @@
 // mui imports
-import { Button, TextField } from '@mui/material'
+import { Button, Divider, TextField } from '@mui/material'
 
 // react imports
-import React from 'react'
+import React, {useState} from 'react'
 
 // rrd imports
 import { Link } from 'react-router-dom'
 
 function Signup() {
+
+    const [formData, setFormData] = useState({
+        name: '',
+        emailAddress: '',
+        location: '',
+        password: '',
+        confirmedPassword: ''
+    })
+
+    const inputChange = (e) => {
+        setFormData((prevState) => ({
+            ...prevState,
+            [e.target.name]: e.target.value
+        }))
+    }
+    
   return (
     <div className='w-full flex flex-col justify-center items-center'>
         <div className='flex flex-col md:w-4/12 w-full gap-y-4 p-3 md:p-0'>
-            <div className='flex flex-col gap-y-4'>
+            <div className='grid grid-cols-2 gap-y-4 gap-x-4'>
 
                 <TextField
                     id="outlined-text-input"
                     label="university name"
                     type="text"
+                    name='name'
+                    className='col-span-2'
+                    value={formData.name}
+                    onChange={inputChange}
                 />
 
                 <TextField
                     id="outlined-email-input"
                     label="email address"
                     type="email"
+                    name='emailAddress'
+                    value={formData.emailAddress}
+                    onChange={inputChange}
                 />
 
                 <TextField
                     id="outlined-text-input"
                     label="state"
                     type="text"
+                    name="location"
+                    value={formData.location}
+                    onChange={inputChange}
                 />
 
                 <TextField
@@ -36,8 +62,23 @@ function Signup() {
                     label="Password"
                     type="password"
                     autoComplete="current-password"
+                    name='password'
+                    value={formData.password}
+                    onChange={inputChange}
+                />
+
+                <TextField
+                    id="outlined-password-input"
+                    label="confirm Password"
+                    type="password"
+                    autoComplete="current-password"
+                    name='confirmedPassword'
+                    value={formData.confirmedPassword}
+                    onChange={inputChange}
                 />
             </div>
+
+            
 
             <Button
                 variant="contained"
@@ -49,6 +90,17 @@ function Signup() {
             <div className='text-right text-xs font-light'>
                 <Link to={`/institution/login`}>Already have an account ? login</Link>
             </div>
+
+            <Divider>or continue with</Divider>
+
+            <Button 
+              variant="contained" 
+              className='bg-[#CCCCCC] text-slate-900 lowercase hover:bg-[#CCCCCC]'
+            >
+              continue with google
+            </Button>
+
+            <p className='text-xs text-center'>by clicking the sign up button you agree to our terms and policies.</p>
             
         </div>  
     </div>
